32791dc863177756cf12161f383f821fa2d99eb4
camila <camila@within.co.kr>
2020-03-17 16:52:36 +0900

    init script

---
+++ src/main/java/com/aaaa/bbbb/application/controller/SlackController.java
@@ -0,0 +1,27 @@
+package com.aaaa.bbbb.application.controller;
+
+import com.aaaa.bbbb.application.response.TestResponse;
+import com.aaaa.bbbb.application.service.TestService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@RestController
+@Validated
+@RequestMapping(value = "/test")
+public class SlackController {
+
+  @Autowired private TestService testService;
+
+  @GetMapping(value = "/test1")
+  @ApiOperation(value = "just select")
+  public TestResponse getTest(
+          @RequestHeader(value = "X-Test", required = false) String head,
+          @RequestParam @NotNull @Min(1) Long id) {
+    return testService.getTest(id);
+  }
+}
---
+++ src/main/java/com/aaaa/bbbb/application/service/SlackService.java
@@ -0,0 +1,25 @@
+package com.aaaa.bbbb.application.service;
+
+import com.aaaa.bbbb.application.response.TestResponse;
+import com.aaaa.bbbb.domain.entity.TestEntity;
+import com.aaaa.bbbb.domain.service.TestDomainService;
+import com.aaaa.bbbb.infrastructure.SlackNotifier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class SlackService {
+  @Autowired private TestDomainService testDomainService;
+
+  @Autowired private SlackNotifier slackNotifier;
+
+  @Transactional(readOnly = true)
+  public TestResponse getTest(Long id) {
+    TestEntity test = testDomainService.getTest(id);
+    TestResponse testResponse = TestResponse.builder().testEntity(test).build();
+
+    slackNotifier.notify(testResponse.getName());
+    return testResponse;
+  }
+}
